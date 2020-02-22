f4de3cc18e76bbc0e4b6385df6fb31395ff02c51
Matahantu <61174912+Matahantu@users.noreply.github.com>
2020-02-22 18:57:49 +0700

    Update tembak.php

--- tembak.php
+++ tembak.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Asia/Jakarta');
 include "function.php";
 echo color("red"," ===========================\n");
 echo color("red","| Auto create Gojek X Redeem voucher |\n");
-echo color("red","| Creator : Calon Sarjana            |\n");
+echo color("red","| Creator : Agus Syahputra            |\n");
 echo "| Version : Premium X pertalite      |\n";
 echo "| Time    :".date('[d-m-Y] [H:i:s]')."   |\n";
 echo " ===========================\n";
